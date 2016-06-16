66287afc1a75dd57c2a44edb8b67a0f3e2ad6bbb
Rosario Sahonero <rosariomrsm11@gmail.com>
2016-06-15 23:48:41 -0400

    Proyecto Green Garden

---
+++ mygarden/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mygarden project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mygarden.settings")
+
+application = get_wsgi_application()
