ad968804364126e5bd56fa2a5364c800365e7add
Rosario Sahonero <rosariomrsm11@gmail.com>
2016-07-16 01:15:27 -0400

    Se ha agregado celery para la creacion de tareas

---
+++ greengarden/tasks.py
@@ -0,0 +1,15 @@
+from celery.tasks.schedules import crontab
+from celery.decorators import periodic_task
+from celery.utils.log import get_task_logger
+
+
+logger = get_task_logger(__name__)
+
+@periodic_task(
+    run_every=(crontab(minute='*/5')),
+    name='task_monitorizar_condiciones_atmosfericas',
+    ignore_result=False
+)
+def task_monitorizar_condiciones_atmosfericas():
+    print("Hello World")
+    logger.info("Monitorizacion completada")
