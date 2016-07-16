cf21fc011b529ae73711b3b46948788e9f32b2c9
Rosario Sahonero <rosariomrsm11@gmail.com>
2016-07-16 11:43:50 -0400

    Se ha corregido un error en el import

--- mygarden/settings.py
+++ mygarden/settings.py
@@ -126,6 +126,7 @@ STATIC_URL = '/static/'
 BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_IMPORTS = ('greengarden.tasks',)
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'America/La_Paz'
