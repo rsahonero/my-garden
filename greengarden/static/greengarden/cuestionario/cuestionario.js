7f1f4aa50597a766743bb010271948b4644f73ae
Rosario Sahonero <rosariomrsm11@gmail.com>
2016-10-01 22:06:18 -0400

    Se ha cambiado el color de los botones

---
+++ greengarden/static/greengarden/cuestionario/cuestionario.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+
+    $('.btn-default').on('mousedown', function () {
+        $(this).css('background-color', '#009688');
+        $(this).css('border-color', '#009688');
+        $(this).css('color', '#FFF');
+    });
+})();
