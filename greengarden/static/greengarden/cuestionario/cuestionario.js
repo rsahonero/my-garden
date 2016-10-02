0bafac203e01866ec49cf9f794d3c6e9616954aa
Rosario Sahonero <rosariomrsm11@gmail.com>
2016-10-02 12:51:46 -0400

    Validación básica

--- greengarden/static/greengarden/cuestionario/cuestionario.js
+++ greengarden/static/greengarden/cuestionario/cuestionario.js
@@ -6,4 +6,13 @@
         $(this).css('border-color', '#009688');
         $(this).css('color', '#FFF');
     });
+
+    $('#cuestionario').submit(function (e) {
+        var valor = $('input[name=valor]:checked').val();
+        if (valor) {
+            return true;
+        } else {
+            return false;
+        }
+    });
 })();
