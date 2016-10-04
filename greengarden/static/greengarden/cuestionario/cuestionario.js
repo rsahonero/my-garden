f15ab989978ecdc09b9302d3849fc145d1d0fc71
Rosario Sahonero <rosariomrsm11@gmail.com>
2016-10-03 22:23:14 -0400

    Mejora de la validación Ahora la validación muestra un mensaje

--- greengarden/static/greengarden/cuestionario/cuestionario.js
+++ greengarden/static/greengarden/cuestionario/cuestionario.js
@@ -10,9 +10,15 @@
     $('#cuestionario').submit(function (e) {
         var valor = $('input[name=valor]:checked').val();
         if (valor) {
+            $('#error-cuestionario').hide();
             return true;
         } else {
+            $('#error-cuestionario').show();
             return false;
         }
+    });
+
+    $('.alert .close').click(function () {
+        $(this).parent().hide();
     });
 })();
